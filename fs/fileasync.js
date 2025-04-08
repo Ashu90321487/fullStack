9d87f673e15643227eae9f04190ee044232cef9a
Ashutosh <a90321487@gmail.com>
2025-04-08 20:44:04 +0530

    add alk

--- fs/fileasync.js
+++ fs/fileasync.js
@@ -1,11 +1,17 @@
+import { log } from "console";
 import fs from "fs/promises";
-
 const asyncwriter = async()=>{
-  await  fs.writeFile("./dataasync.csv","this is a async file.");
-  console.log("file has been written successfully.(async");
-} catch (error){
-    console.log("unable to write file.(async)",error);
- }
- 
+     try{await fs.writeFile("./dataasync.csv","This is the async file.");
+         console.log("File has been written successfully.(async)"); 
+     }catch(error){
+         console.log("unable to write file.(async)",error);
+    }
+}
+const asyncreader = async() =>{
+        const fileData = await fs.readFile("./dataasync.csv","utf-8");
+        return fileData;
+}
     
 asyncwriter();
+const data = asyncreader();
+console.log(data);
